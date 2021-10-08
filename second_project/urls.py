8d7e8424192a6b63ec679c9fa3075a0ac6c684e3
Begimain <begimaibaikabylova@gmail.com>
2021-10-08 18:41:46 +0300

    Домашняя работа

--- second_project/urls.py
+++ second_project/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from second_project.views import index, index2, index3, index4, index5
+
 urlpatterns = [
+    path('', index, name='index'),
+    path('2/', index2, name='index2'),
+    path('3/', index3, name='index3'),
+    path('4/', index4, name='index4'),
+    path('5/', index5, name='index5'),
     path('admin/', admin.site.urls),
 ]
---
+++ second_project/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+def index(request):
+    name = 'Baikabylova Begimai'
+    return render(request, 'index.html', locals())
+
+def index2(request):
+    name = 'Djusupov Uson '
+    return render(request, 'index2.html', locals())
+
+def index3(request):
+    name = 'Mamytova Aizura'
+    return render(request, 'index2.html', locals())
+
+def index4(request):
+    name = 'Baikabylov Omurbek'
+    return render(request, 'index2.html', locals())
+
+def index5(request):
+    name = 'Baikabylov Nurelbek'
+    return render(request, 'index2.html', locals())
